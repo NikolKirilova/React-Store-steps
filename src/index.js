a121586be3c51b9052daf1e4c6aa29bf9d55ef95
NikolKirilova <kirilova@mail.bg>
2022-02-03 20:33:08 +0200

    add auth 0

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import { ProductsProvider } from './context/products_context'
-import { FilterProvider } from './context/filter_context'
-import { CartProvider } from './context/cart_context'
-import { UserProvider } from './context/user_context'
-import { Auth0Provider } from '@auth0/auth0-react'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ProductsProvider } from "./context/products_context";
+import { FilterProvider } from "./context/filter_context";
+import { CartProvider } from "./context/cart_context";
+import { UserProvider } from "./context/user_context";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+//dev-0n5e17o6.us.auth0.com
+//zhZSxfDGFUnxpF8jFgyDwxUpFDQg1z1V
 
 ReactDOM.render(
-<ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-      <App />
-      </CartProvider>
-    </FilterProvider>
-</ProductsProvider>
+  <Auth0Provider
+    domain="dev-0n5e17o6.us.auth0.com"
+    clientId="zhZSxfDGFUnxpF8jFgyDwxUpFDQg1z1V"
+    redirectUri={window.location.origin}
+    cacheLocation="localstorage"
+  >
+    <ProductsProvider>
+      <FilterProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterProvider>
+    </ProductsProvider>
+  </Auth0Provider>,
 
-
-, document.getElementById('root'))
+  document.getElementById("root")
+);
