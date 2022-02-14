701a6acba83aa6381344890f885b0a6cc113b13b
NikolKirilova <kirilova@mail.bg>
2022-02-14 22:32:46 +0200

    auth context

--- src/pages/AuthWrapper.js
+++ src/pages/AuthWrapper.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import styled from 'styled-components'
+import AuthForm from '../components/AuthForm'
 
 const AuthWrapper = () => {
-  return <h4>AuthWrapper Component</h4>
+  return <AuthForm/>
 }
 
 const Wrapper = styled.section`
-  min-height: 100vh;
+  min-height: 200vh;
   display: grid;
   place-items: center;
 `
