20f080e7e18220db3726c6014c8f7496ad5b3c5b
NikolKirilova <kirilova@mail.bg>
2022-03-09 20:19:08 +0200

    add form steps

---
+++ src/components/FormStepOne.js
@@ -0,0 +1,192 @@
+import React from "react";
+import axios from "axios";
+import { useState, useRef, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import styled from "styled-components";
+import AuthContext from "../context/auth_context";
+import { FirstTab, SecondTab } from "../components";
+
+const FormStepOne = () => {
+  const history = useHistory();
+  
+
+  const authCtx = useContext(AuthContext);
+  // const id = localStorage.getItem("localId");
+
+  const [isLogin, setIsLogin] = useState(true);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+
+  const [activeTab, setActiveTab] = useState("tab1");
+   //  Functions to handle Tab Switching
+   const handleTab1 = () => {
+    // update the state to tab1
+    setActiveTab("tab1");
+  };
+  const handleTab2 = () => {
+    // update the state to tab2
+    setActiveTab("tab2");
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+  
+
+    setIsLoading(true);
+    // axios({
+    //   method: "post",
+    //   url: "http://127.0.0.1:8000/api/users",
+    //   data: {
+    //     first_name: firstName,
+    //     last_name: lastName,
+    //     email: enteredEmail,
+    //     password: enteredPassword,
+    //     user_id: userId,
+    //   },
+    // }).then(
+    //   (response) => {
+    //     console.log(response);
+    //   },
+    //   (error) => {
+    //     console.log(error);
+    //   }
+    // );
+  };
+
+  return (
+    <Wrapper className="">
+      <section className="auth">
+         
+        <div className="personal-info">
+          <div className="personal-info-tabs">
+            <div className="new-customer-title">
+              <button className={activeTab === "tab1" ? "active" : ""}  onClick={handleTab1}>
+                New customer
+              </button>
+            </div>
+            <div  >
+              <button className={activeTab === "tab2" ? "active" : ""} onClick={handleTab2}>
+                Sign in
+                </button>
+            </div>
+          </div>
+            <div>{activeTab === "tab1" ? <FirstTab /> : <SecondTab />}  </div>
+        </div>
+      </section>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  .auth {
+    margin: 3rem auto;
+    // width: 95%;
+    // max-width: 25rem;
+    border-radius: 6px;
+    // box-shadow: 0 1px 4px rgba(0, 0, 0, 0.2);
+    padding: 1rem;
+    text-align: center;
+    color: #212529;
+  }
+  .personal-info-tabs{
+    margin-bottom: 20px;
+  }
+  
+  .personal-info-tabs button{
+    width: 200px;
+    height: 40px;
+    border: 1px solid #999;
+    border-bottom: none;
+    color:#d9d9d9;
+    background :#f7f7f7;
+  }
+  .personal-info-tabs button.active{
+    background:#fff;
+    color:#000;
+  }
+  .tab {
+    color: #666;
+    background: #000;
+  }
+
+  .auth h1 {
+    text-align: left;
+    letter-spacing: 0.1px;
+  }
+  .personal-info {
+    width: 380px;
+  }
+
+  .personal-info-tabs {
+    display: flex;
+    justify-content: space-around;
+  }
+
+  .control {
+    margin-bottom: 0.5rem;
+    display: flex;
+  }
+
+  .control label {
+    flex: 0 0 35%;
+    font-weight: bold;
+    margin-bottom: 0.5rem;
+  }
+
+  .control input {
+    font: inherit;
+    background-color: #f7f7f7;
+    color: #38015c;
+    border-radius: 20px;
+    border: none;
+    width: 100%;
+    text-align: left;
+    padding: 0.25rem;
+  }
+  .control input:focus {
+    color: #495057;
+    background-color: #fff;
+    border-color: #404040;
+    box-shadow: 0 0 0 0.2rem rgb(0 0 0 /25%);
+  }
+  .control input:focus-visible {
+    border: none;
+  }
+
+  .actions {
+    margin-top: 1.5rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .actions button {
+    cursor: pointer;
+    font: inherit;
+    color: white;
+    background-color: #9f5ccc;
+    border: 1px solid #9f5ccc;
+    border-radius: 4px;
+    padding: 0.5rem 2.5rem;
+  }
+
+  .actions button:hover {
+    background-color: #873abb;
+    border-color: #873abb;
+  }
+
+  .actions .toggle {
+    margin-top: 1rem;
+    background-color: transparent;
+    color: #9f5ccc;
+    border: none;
+    padding: 0.15rem 1.5rem;
+  }
+
+  .actions .toggle:hover {
+    background-color: transparent;
+    color: #ae82cc;
+  }
+`;
+export default FormStepOne;
