03fb825e4069bd9a2f7e977c7275486860ae604a
NikolKirilova <kirilova@mail.bg>
2022-03-10 20:08:32 +0200

    edit first step

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -10,14 +10,17 @@ const AuthForm = () => {
    
 
  const authCtx = useContext(AuthContext);
+ const isLoggedIn = authCtx.isLoggedIn;
 
-  const [isLogin, setIsLogin] = useState(true);
+   
   const [isLoading, setIsLoading] = useState(false);
   const [isActive, setIsActive] = useState("tab1");
 
-  const switchAuthModeHandler = () => {
-    setIsLogin((prevState) => !prevState);
-  };
+  const [value, setValue] = useState(false);
+
+ const handleChange=(newValue)=>{
+   setValue(newValue);
+ }
    
   const toggleHandler1 = () => {
       setIsActive("tab1")
@@ -31,6 +34,62 @@ const toggleHandler3 = () => {
 const toggleHandler4 = () => {
   setIsActive("tab4")
 }  
+
+
+const succeedStepOneHandler=(userData)=>{
+
+  fetch(
+    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAi1TLxgD9IkUz6kypN0VRbDGqalCWdPLM",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        ...userData,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
+    .then((res) => {
+      // setIsLoading(false);
+      if (res.ok) {
+        return res.json();
+      } else {
+        return res.json().then((data) => {
+          //show an error modal
+          // console.log(data);
+          let errorMessage = "Authentication failed!";
+          if (data && data.error && data.error.message) {
+            errorMessage = data.error.message;
+          }
+          // alert(errorMessage);
+          throw new Error(errorMessage);
+        });
+      }
+    })
+    .then((data) => {
+      console.log(data);
+      console.log(data.localId);
+      console.log(data.idToken);
+
+      const expirationTime = new Date(
+        new Date().getTime() + +data.expiresIn * 1000
+      );
+      authCtx.login(
+        data.idToken,
+        expirationTime.toISOString(),
+        data.email,
+        data.localId
+      );
+
+      // history.replace('/');
+    })
+    .catch((err) => {
+      alert(err.message);
+    });
+
+}
 
   const submitHandler = (event) => {
     event.preventDefault();
@@ -100,7 +159,7 @@ const toggleHandler4 = () => {
         <div className="header-box" onClick={toggleHandler1}>
         <h4>1. Personal Information</h4>
           </div>
-          {isActive === "tab1" && <FormStepOne/>}          
+          {isActive === "tab1" && <FormStepOne onSuccess={succeedStepOneHandler}/>}          
          <div className="header-box" onClick={toggleHandler2}>
            <h4>2.Addresses</h4>
           </div>
--- src/components/FirstTab.js
+++ src/components/FirstTab.js
@@ -4,102 +4,112 @@ import { useHistory } from "react-router-dom";
 import styled from "styled-components";
 import AuthContext from "../context/auth_context";
 
-const FirstTab =()=> {
+const FirstTab = (props) => {
+  const firstNameInputRef = useRef();
+  const lastNameInputRef = useRef();
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
-    const firstNameInputRef = useRef();
-    const lastNameInputRef = useRef();
-    const emailInputRef = useRef();
-    const passwordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+  // const id = localStorage.getItem("localId");
 
-    const authCtx = useContext(AuthContext);
-    // const id = localStorage.getItem("localId");
+  const [isLogin, setIsLogin] = useState(true);
 
-    const [isLogin, setIsLogin] = useState(true);
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
 
-    const switchAuthModeHandler = () => {
-        setIsLogin((prevState) => !prevState);
-      };
+  const [disabled, setDisable] = React.useState(true);
 
-    const submitHandler = (event) => {
-        event.preventDefault();
-        const firstName = firstNameInputRef.current.value;
-        const lastName = lastNameInputRef.current.value;
-        const enteredEmail = emailInputRef.current.value;
-        const enteredPassword = passwordInputRef.current.value;
-        // const userId = id;
-      
-    
-        //add validation
-    }
-  
-    return (
-        <form onSubmit={submitHandler}>
-        <div className="control">
-            <label htmlFor="firstName">First Name</label>
-            <input type="text" id="firstName" required ref={firstNameInputRef} />
-          </div>
-          <div className="control">
-            <label htmlFor="name">Second Name</label>
-            <input type="text" id="name" required ref={lastNameInputRef} />
-          </div>
-          <div className="control">
-            <label htmlFor="email">Your Email</label>
-            <input type="email" id="email" required ref={emailInputRef} />
-          </div>
-          <div className="control">
-            <label htmlFor="password">Your Password</label>
-            <input
-              type="password"
-              id="password"
-              required
-              ref={passwordInputRef}
-            />
-          </div>
-          <div className="actions">
-            {/* {!isLoading && (
+  const submitHandler = (event) => {
+    event.preventDefault();
+    let enteredFirstName = firstNameInputRef.current.value;
+    let enteredLastName = lastNameInputRef.current.value;
+    let enteredEmail = emailInputRef.current.value;
+    let enteredPassword = passwordInputRef.current.value;
+    // const userId = id;
+
+    const userData = {
+      email: enteredEmail,
+      password: enteredPassword,
+      firstName: enteredFirstName,
+      lastName: enteredLastName,
+    };
+
+    props.onRegisterUser(userData);
+
+
+    firstNameInputRef.current.value = "";
+    lastNameInputRef.current.value = "";
+    emailInputRef.current.value = "";
+    passwordInputRef.current.value = "";
+
+     
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="control">
+        <label htmlFor="firstName">First Name</label>
+        <input type="text" id="firstName" required ref={firstNameInputRef} />
+      </div>
+      <div className="control">
+        <label htmlFor="name">Second Name</label>
+        <input type="text" id="name" required ref={lastNameInputRef} />
+      </div>
+      <div className="control">
+        <label htmlFor="email">Your Email</label>
+        <input type="email" id="email" required ref={emailInputRef} />
+      </div>
+      <div className="control">
+        <label htmlFor="password">Your Password</label>
+        <input type="password" id="password" required ref={passwordInputRef} />
+      </div>
+      <div className="actions">
+        {/* {!isLoading && (
               <button>Continue </button>
             )}
             {isLoading && <p>Sending request ...</p>} */}
-            <button
-            //   type="button"
-            //   className="toggle"
-            //   onClick={switchAuthModeHandler}
-            >
-              {isLogin ? "Create new account" : "Login with existing account"}
-            </button>
-          </div>
-        </form>)
-   
-    
-}
+        <button
+        //   type="button"
+        //   className="toggle"
+        //   onClick={switchAuthModeHandler}
+        >
+          {/* {isLogin ? "Create new account" : "Login with existing account"} */}
+          Continue
+        </button>
+      </div>
+    </form>
+  );
+};
 const Wrapper = styled.section`
   .auth {
     margin: 3rem auto;
     // width: 95%;
     // max-width: 25rem;
-    border-radius: 6px;     
+    border-radius: 6px;
     box-shadow: 0 1px 4px rgba(0, 0, 0, 0.2);
     padding: 1rem;
     text-align: center;
-    color:#212529;
+    color: #212529;
   }
 
   .auth h1 {
     text-align: left;
     letter-spacing: 0.1px;
   }
-  .personal-info{
-    width:380px;
+  .personal-info {
+    width: 380px;
   }
 
-  .personal-info-tabs{
+  .personal-info-tabs {
     display: flex;
     justify-content: space-around;
   }
 
   .control {
     margin-bottom: 0.5rem;
-    display:flex;
+    display: flex;
   }
 
   .control label {
@@ -118,14 +128,14 @@ const Wrapper = styled.section`
     text-align: left;
     padding: 0.25rem;
   }
-  .control input:focus{
-    color:#495057;
-    background-color:#fff;
-    border-color:#404040;
+  .control input:focus {
+    color: #495057;
+    background-color: #fff;
+    border-color: #404040;
     box-shadow: 0 0 0 0.2rem rgb(0 0 0 /25%);
   }
-  .control input:focus-visible{
-    border:none;
+  .control input:focus-visible {
+    border: none;
   }
 
   .actions {
@@ -139,8 +149,8 @@ const Wrapper = styled.section`
     cursor: pointer;
     font: inherit;
     color: white;
-    background-color: #9f5ccc;
-    border: 1px solid #9f5ccc;
+    background-color: #f5554c;
+    border-color: #f5554c;
     border-radius: 4px;
     padding: 0.5rem 2.5rem;
   }
@@ -161,6 +171,18 @@ const Wrapper = styled.section`
   .actions .toggle:hover {
     background-color: transparent;
     color: #ae82cc;
+  }
+  button::disabled {
+    background-color: #999 !important;
+    border-color: #999 !important;
+  }
+  button.button-disabled {
+    background-color: #999;
+    border-color: #999;
+  }
+  .button-normal {
+    background-color: #9f5ccc;
+    border: 1px solid #9f5ccc;
   }
 `;
 export default FirstTab;
